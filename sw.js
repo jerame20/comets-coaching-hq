1573b99c00339f71e07897356881a09f8740bba2
Darwin <darwin@Mac.localdomain>
2026-07-21 00:26:23 -0500

    Remove Home photo and repair night mode contrast

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "comets-hq-v14";
-const ASSETS = ["./", "./index.html", "./styles.css?v=14", "./data.js?v=10", "./app.js?v=15", "./manifest.json", "./icon.svg", "./assets/comets-team-reference.png"];
+const CACHE = "comets-hq-v15";
+const ASSETS = ["./", "./index.html", "./styles.css?v=15", "./data.js?v=10", "./app.js?v=16", "./manifest.json", "./icon.svg"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", (event) => {
